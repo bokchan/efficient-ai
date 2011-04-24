8a96464cc9e4eeaa1118125a2fa4993092681c8c
mm.floistrup@gmail.com <mm.floistrup@gmail.com@9814922a-5345-4fef-9896-1177c7947b26>
2011-04-24 15:48:57 +0000

    new test (still not working)

--- Sudoku/trunk/src/ShowSudoku.java
+++ Sudoku/trunk/src/ShowSudoku.java
@@ -2,9 +2,11 @@ import javax.swing.*;
 
 public class ShowSudoku {
 	public static void main(String[] arg){
+		
 		ISudokuSolver s = new SudokuSolver();
 		s.setup(3);
-		int [][] puz= new int[9][9];
+	
+	/**	int [][] puz= new int[9][9];
 		puz[1][0]= 7;
 		puz[1][1]= 4;
 		puz[4][1]= 9;
@@ -16,14 +18,18 @@ public class ShowSudoku {
 		puz[6][6]= 6;
 		puz[7][7]= 4;
 		puz[7][8]= 2;
-	
+	//**/
 		
-		s.readInPuzzle(puz);
+		//s.readInPuzzle(puz);
 		//System.out.println(s.getPuzzle());
 		
-		//s.solve();
+		//s.setValue(4,1,9);
+		s.setValue(1,1,4);
+		s.setValue(7,8,2);
+		//s.setValue(7,7,4);
+		s.solve();
 		
-		SudokuGUI g = new SudokuGUI(s);
+	/**	SudokuGUI g = new SudokuGUI(s);
 
 		// Setup of the frame containing the puzzle
 		JFrame f = new JFrame();
@@ -32,7 +38,7 @@ public class ShowSudoku {
 		f.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
 		f.getContentPane().add(g);    
 		f.setVisible(true);
-		
+		**/
 		
 	}
 }
