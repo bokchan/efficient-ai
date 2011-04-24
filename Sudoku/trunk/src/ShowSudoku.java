7e78f540aa4ba9132dd3faf652b48011dcc528aa
mm.floistrup@gmail.com <mm.floistrup@gmail.com@9814922a-5345-4fef-9896-1177c7947b26>
2011-04-24 14:56:44 +0000

    Test of preexisting sudoku (solve not working)

--- Sudoku/trunk/src/ShowSudoku.java
+++ Sudoku/trunk/src/ShowSudoku.java
@@ -4,7 +4,25 @@ public class ShowSudoku {
 	public static void main(String[] arg){
 		ISudokuSolver s = new SudokuSolver();
 		s.setup(3);
-
+		int [][] puz= new int[9][9];
+		puz[1][0]= 7;
+		puz[1][1]= 4;
+		puz[4][1]= 9;
+		puz[4][2]= 2;
+		puz[4][3]= 1;
+		puz[4][5]= 5;
+		puz[4][8]= 7;
+		puz[0][7]= 9;
+		puz[6][6]= 6;
+		puz[7][7]= 4;
+		puz[7][8]= 2;
+	
+		
+		s.readInPuzzle(puz);
+		//System.out.println(s.getPuzzle());
+		
+		//s.solve();
+		
 		SudokuGUI g = new SudokuGUI(s);
 
 		// Setup of the frame containing the puzzle
@@ -14,6 +32,8 @@ public class ShowSudoku {
 		f.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
 		f.getContentPane().add(g);    
 		f.setVisible(true);
+		
+		
 	}
 }
 
