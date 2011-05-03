361f076f466fefeefe85d2be32416a97401cb6c4
bok.chan@gmail.com <bok.chan@gmail.com@9814922a-5345-4fef-9896-1177c7947b26>
2011-05-03 13:34:21 +0000

    Added example

--- Sudoku/trunk/src/ShowSudoku.java
+++ Sudoku/trunk/src/ShowSudoku.java
@@ -1,11 +1,27 @@
-import javax.swing.*;
+import javax.swing.JFrame;
 
 public class ShowSudoku {
 	public static void main(String[] arg){
 		
 		ISudokuSolver s = new SudokuSolver();
 		s.setup(3);
+		
+		if (arg.length == 1)
+			if (arg[0].equals("example"))
+				s.readInPuzzle(loadExample());
+		
+		SudokuGUI g = new SudokuGUI(s);
+
+		// Setup of the frame containing the puzzle
+		JFrame f = new JFrame();
+		f.setSize(1000,1000);
+		f.setTitle("Sudoku Solver");
+		f.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+		f.getContentPane().add(g);    
+		f.setVisible(true);		
+	}
 	
+	private static int[][] loadExample() {
 		int [][] puz= new int[9][9];
 		puz[0][0]= 9;
 		puz[2][0]= 1;
@@ -35,28 +51,12 @@ public class ShowSudoku {
 		puz[8][7]=1;
 		puz[6][8]=4;
 		puz[8][8]=3;
-	
 		
-		s.readInPuzzle(puz);
-		//System.out.println(s.getPuzzle());
-		
-		//s.setValue(4,1,9);
-		//s.setValue(1,1,4);
-		//s.setValue(7,8,2);
-		//s.setValue(7,7,4);
-		//s.solve();
-		
-		SudokuGUI g = new SudokuGUI(s);
-
-		// Setup of the frame containing the puzzle
-		JFrame f = new JFrame();
-		f.setSize(1000,1000);
-		f.setTitle("Sudoku Solver");
-		f.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-		f.getContentPane().add(g);    
-		f.setVisible(true);
-		
-		
+		return puz;
 	}
+	
+	private static void printHelp() {
+		
+	} 
 }
 
